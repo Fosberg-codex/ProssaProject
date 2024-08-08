aaf66d2548f9043ff2dc6dddecca12665e42e963
Fosberg Addai <83887297+Fosberg-codex@users.noreply.github.com>
2024-08-09 10:46:40 +1200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,59 +1,59 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight(
-			{
-				favicon: '/favicon.ico',
-				title: 'Docs | Prossa',
-				customCss: ['./src/styles/custom.css'],
-				social: {
-				  github: 'https://github.com/Fosberg-codex/prossa'
-				},
-				sidebar: [{
-				  label: 'Introduction',
-				  autogenerate: {
-					directory: 'introduction'
-				  }
-				}, {
-				  label: 'Guide',
-				  items: [
-				  // Each item here is one entry in the navigation menu.
-				  {
-					label: 'Installation',
-					slug: 'user-guide/installation'
-				  }, {
-					label: 'Get started',
-					slug: 'user-guide/gettingstarted'
-				  }, {
-					label: 'Techniques',
-					// slug: 'user-guide/methods'
-					items: [{
-					  label: 'Outlier',
-					  slug: 'user-guide/methods/outliers'
-					}, {
-					  label: 'Missing Values',
-					  slug: 'user-guide/methods/missingvalues'
-					}, {
-					  label: 'Scaling and Encoding',
-					  slug: 'user-guide/methods/scalingencoding'
-					}, {
-					  label: 'All methods',
-					  slug: 'user-guide/methods/allmethods'
-					}]
-				  }, {
-					label: 'Example',
-					slug: 'user-guide/example'
-				  }]
-				}, {
-				  label: 'References',
-				  autogenerate: {
-					directory: 'reference'
-				  }
-				}],
-			  }
-	),
-	],
+  integrations: [starlight({
+    favicon: '/favicon.ico',
+    title: 'Docs | Prossa',
+    customCss: ['./src/styles/custom.css'],
+    social: {
+      github: 'https://github.com/Fosberg-codex/prossa'
+    },
+    sidebar: [{
+      label: 'Introduction',
+      autogenerate: {
+        directory: 'introduction'
+      }
+    }, {
+      label: 'Guide',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Installation',
+        slug: 'user-guide/installation'
+      }, {
+        label: 'Get started',
+        slug: 'user-guide/gettingstarted'
+      }, {
+        label: 'Techniques',
+        // slug: 'user-guide/methods'
+        items: [{
+          label: 'Outlier',
+          slug: 'user-guide/methods/outliers'
+        }, {
+          label: 'Missing Values',
+          slug: 'user-guide/methods/missingvalues'
+        }, {
+          label: 'Scaling and Encoding',
+          slug: 'user-guide/methods/scalingencoding'
+        }, {
+          label: 'All methods',
+          slug: 'user-guide/methods/allmethods'
+        }]
+      }, {
+        label: 'Example',
+        slug: 'user-guide/example'
+      }]
+    }, {
+      label: 'References',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  })],
+  output: "server",
+  adapter: vercel()
 });
